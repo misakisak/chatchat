ed6725c142c39d1231a26cc8e4513fa4003e5977
Kotomi Fujita <g2120533@is.ocha.ac.jp>
2024-03-21 21:23:16 +0900

    衝突を直した

--- lib/ChatPage.dart
+++ lib/ChatPage.dart
@@ -4,17 +4,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 //絵文字のためにimportした
 import 'package:emoji_regex/emoji_regex.dart';
 // import 'package:flutter/src/widgets/framework.dart';
 import 'package:intl/intl.dart';
 //もし 2022/01/01 21:55:31 のように表示したいのであれば
 //DateFormat('yyyy/MM/dd HH:mm:ss')
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+//言語変更のため
 import 'package:chat/main.dart';
 import 'package:chat/post.dart';
 import 'package:chat/my_page.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
@@ -23,94 +24,6 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-
-     @override
-     Widget build(BuildContext context) {
-          return Scaffold(
-               appBar: AppBar(
-                    title: Text(AppLocalizations.of(context).title),
-                    // actions プロパティにWidgetを与えると右端に表示されます。
-                    actions: [
-                         // tap 可能にするために InkWell を使います。
-                         InkWell(
-                              onTap: () {
-                                   Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                             builder: (context) {
-                                                  return const MyPage();
-                                             },
-                                        ),
-                                   );
-                              },
-                              child: CircleAvatar(
-                                   backgroundImage: NetworkImage(
-                                        FirebaseAuth.instance.currentUser!.photoURL!,
-                                   ),
-                              ),
-                         )
-                    ],
-               ),
-               body: Column(
-                    children: [
-                         Expanded(
-                              child: StreamBuilder<QuerySnapshot<Post>>(
-                                   // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
-                                   // stream: postsReference.snapshots(),
-                                   //下のやつでpostの表示を時効列に変えている
-                                   stream: postsReference.orderBy('createdAt').snapshots(),
-                                   // ここで受け取っている snapshot に stream で流れてきたデータが入っています。
-                                   builder: (context, snapshot) {
-                                        // docs には Collection に保存されたすべてのドキュメントが入ります。
-                                        // 取得までには時間がかかるのではじめは null が入っています。
-                                        // null の場合は空配列が代入されるようにしています。
-                                        final docs = snapshot.data?.docs ?? [];
-                                        return ListView.builder(
-                                             itemCount: docs.length,
-                                             itemBuilder: (context, index) {
-                                                  // data() に Post インスタンスが入っています。
-                                                  // これは withConverter を使ったことにより得られる恩恵です。
-                                                  // 何もしなければこのデータ型は Map になります。
-                                                  final post = docs[index].data();
-                                                  return Text(post.text);
-                                             },
-                                        );
-                                   },
-                              ),
-                         ),
-                         TextFormField(
-                              onFieldSubmitted: (text) {
-                                   // まずは user という変数にログイン中のユーザーデータを格納します
-                                   final user = FirebaseAuth.instance.currentUser!;
-
-                                   final posterId = user.uid; // ログイン中のユーザーのIDがとれます
-                                   final posterName = user.displayName!; // Googleアカウントの名前がとれます
-                                   final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
-
-                                   // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
-                                   // doc の引数を空にするとランダムなIDが採番されます
-                                   final newDocumentReference = postsReference.doc();
-
-                                   final newPost = Post(
-                                        text: text,
-                                        createdAt: Timestamp.now(), // 投稿日時は現在とします
-                                        // createdAt: DateTime.now().millisecondsSinceEpoch,
-                                        // createdAt: Timestamp.now(),
-                                        posterName: posterName,
-                                        posterImageUrl: posterImageUrl,
-                                        posterId: posterId,
-                                        reference: newDocumentReference,
-                                   );
-
-                                   // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
-                                   // 引数として Post インスタンスを渡します。
-                                   // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
-                                   newDocumentReference.set(newPost);
-                              },
-                         ),
-                    ]
-               ),
-          );
-     }
 
   Future<void> sendPost(String text) async {
     // まずは user という変数にログイン中のユーザーデータを格納します
@@ -194,7 +107,7 @@ class _ChatPageState extends State<ChatPage> {
     
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('チャット'),
+          title: Text(AppLocalizations.of(context).title),
           // actions プロパティにWidgetを与えると右端に表示されます。
           actions: [
             // tap 可能にするために InkWell を使います。
